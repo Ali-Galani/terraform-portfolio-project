1531d14cf32296052dd44efbc74a91f6b0d12e8b
Ali Galani <Mohsin.galani@gmail.com>
2026-06-19 14:54:44 -0700

    syntax issues.

--- ag-blog/terraform-js/Main.tf
+++ ag-blog/terraform-js/Main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "nextjs_bucket" {
 resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_controls" {
   bucket = aws_s3_bucket.nextjs_bucket.id
     rule{
-    object_ownership = "BucketOwnerPrefered"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
