e7ea155a2d6edd76a64dfbd0cde0da3e19fa26e1
Ali-Galani <mohsin.galani@gmail.com>
2026-06-19 16:36:37 -0700

    Delete ag-blog directory

--- ag-blog/terraform-js/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "ag-my-tf-website-state"
-    key = "global/s3/terraform/tfstate"
-    region = "us-west-2"
-    dynamodb_table = "my-db-website-table" 
-  }
-}
