2d7ccdedddc5be92e2de1914f585559468c3cf0b
Ali Galani <Mohsin.galani@gmail.com>
2026-06-19 14:49:54 -0700

    fixed state file.

--- ag-blog/terraform-js/state.tf
+++ ag-blog/terraform-js/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    Bucket = "ag-my-tf-website-state"
+    bucket = "ag-my-tf-website-state"
     key = "global/s3/terraform/tfstate"
     region = "us-west-2"
     dynamodb_table = "my-db-website-table" 
